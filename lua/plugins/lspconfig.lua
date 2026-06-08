4d93e7984eb3767bd589fadb2971b0be06f492ad
Moritz Ruge <ruge.moritz@gmail.com>
2026-06-08 23:23:12 +0200

    lsp for C is working

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,21 @@
 return {
+    {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+	    "hrsh7th/cmp-nvim-lsp",
+	},
+	config = function()
+	    -- capabilities für autocomplete
+	    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+	    vim.lsp.config("clangd", {
+		capabilities = capabilities,
+		cmd = { "clangd" },
+		filetypes = { "c", "cpp" },
+		root_dir = vim.fs.root(0, { "compile_commands.json", ".git" }),
+	    })
+
+	    vim.lsp.enable("clangd")
+	end,
+    },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,20 +1,6 @@
 return {
     { --mason lsp installer
 	"mason-org/mason.nvim",
-	build = ":MasonUpdate",
-	opts = {
-	    ensure_installed = {
-		"lua_ls",
-		"tinymist",
-	    },
-	},
-	config = function(_, opts)
-	    require("mason").setup(opts)
-	end,
-    },
-
-    { -- mason - lapconfig bridge
-	"mason-org/mason-lspconfig.nvim",
 	opts = {},
     },
 }
