8048ea2176814150dfa0bb790635491e4172f649
Moritz Ruge <ruge.moritz@gmail.com>
2026-06-09 19:08:53 +0200

    tinymist im lsp hinzugefügt

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,3 +1,15 @@
+local map = vim.keymap.set
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+-- Typst keybinds
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "typst",
+    callback = function()
+	map("n", "<leader>tw", "<cmd>TypstWatch<CR>", { desc = "Typst watch"})
+	map("n", "<leader>tc", "<cmd>TypstCompile<CR>", { desc = "Typst compile"})
+	map("n", "<leader>tp", "<cmd>TypstPreview<CR>", { desc = "Typst Preview"})
+    end,
+})
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,15 +7,24 @@ return {
 	config = function()
 	    -- capabilities für autocomplete
 	    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+	    
+	    -- clangd
 	    vim.lsp.config("clangd", {
 		capabilities = capabilities,
 		cmd = { "clangd" },
 		filetypes = { "c", "cpp" },
 		root_dir = vim.fs.root(0, { "compile_commands.json", ".git" }),
 	    })
-
 	    vim.lsp.enable("clangd")
+
+	    -- typst
+	    vim.lsp.config("tinymist", {
+		capabilities = capabilities,
+		cmd = { "tinymist" },
+		filetypes = { "typst" },
+		root_dir = vim.fs.root(0, { ".git" }),
+	    })
+	    vim.lsp.enable("tinymist")
 	end,
     },
 }
