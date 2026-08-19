0dcb072dc00c7234f3a77c220f51c33e811a2dc3
Moritz Ruge <ruge.moritz@gmail.com>
2026-08-19 11:02:50 +0200

    put nvim options out of keybinds -> options

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -3,14 +3,6 @@ local map = vim.keymap.set
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
-vim.opt.clipboard = "unnamedplus"
-vim.opt.scrolloff = 20
-
--- indentation
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 8
-vim.opt.softtabstop = 8
-vim.opt.expandtab = true
 
 -- Typst keybinds
 vim.api.nvim_create_autocmd("FileType", {
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,12 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
 
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 20
+
+-- indentation
+vim.opt.tabstop = 8
+vim.opt.shiftwidth = 8
+vim.opt.softtabstop = 8
+vim.opt.expandtab = true
